5239f3edd389643efd86d742139c1f7bc5a18650
赵孙超 <1493312415@qq.com>
2025-06-27 15:12:27 +0800

    fix: 更新视频解析API的域名地址

--- api/videoAnalysis.js
+++ api/videoAnalysis.js
@@ -2,7 +2,7 @@ import qs from 'qs'; // qs.stringify(data)
 import { sendApiRequest } from '../utils/axiosEncapsulation.js'
 // 视频解析
 export async function sp_jx_Analysis(data) {
-  const res = await sendApiRequest(`https://www.hhlqilongzhu.cn/api/sp_jx/sp.php?${qs.stringify({
+  const res = await sendApiRequest(`https://api.dragonlongzhu.cn/api/sp_jx/sp.php?${qs.stringify({
     ...data
   })}`)
   return res
