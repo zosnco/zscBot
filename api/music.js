fa4f8806c90c5a05bf4510a9c317ec7f52137540
感觉唔 <1493312415@qq.com>
2025-08-14 21:14:03 +0800

    fix: 修复音乐API请求URL中的双斜杠问题

--- api/music.js
+++ api/music.js
@@ -1,7 +1,7 @@
 import qs from 'qs'; // qs.stringify(data)
 import { sendApiRequest } from '../utils/axiosEncapsulation.js'
 export async function dg_kugouSQ(data) {
-  const res = await sendApiRequest(`https://www.hhlqilongzhu.cn//api/dg_qishuimusic.php?${qs.stringify({
+  const res = await sendApiRequest(`https://www.hhlqilongzhu.cn/api/dg_qishuimusic.php?${qs.stringify({
     msg: '',
     n: 1,
     type: 'json',
