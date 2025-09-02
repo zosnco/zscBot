bf4a0c6b780d5ced0e385ebdae285bd9cd744627
赵孙超 <1493312415@qq.com>
2025-09-02 12:14:22 +0800

    fix(音乐API): 更新点歌接口URL并调整响应数据字段
    将点歌接口URL更新为新的端点，并调整代码以匹配新的响应数据结构

--- api/music.js
+++ api/music.js
@@ -1,7 +1,8 @@
 import qs from 'qs'; // qs.stringify(data)
 import { sendApiRequest } from '../utils/axiosEncapsulation.js'
 export async function dg_kugouSQ(data) {
-  const res = await sendApiRequest(`https://www.hhlqilongzhu.cn/api/dg_kugouSQ.php?${qs.stringify({
+  const res = await sendApiRequest(`https://sdkapi.hhlqilongzhu.cn/api/juhe_dgmusic?${qs.stringify({
+    key:'',
     msg: '',
     n: 1,
     type: 'json',
