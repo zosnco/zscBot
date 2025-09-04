ee29965a6b4ea6195bc60190cfd49d38350f52c5
赵孙超 <1493312415@qq.com>
2025-09-04 10:00:36 +0800

    1

--- api/music.js
+++ api/music.js
@@ -2,7 +2,7 @@ import qs from 'qs'; // qs.stringify(data)
 import { sendApiRequest } from '../utils/axiosEncapsulation.js'
 export async function dg_kugouSQ(data) {
   const res = await sendApiRequest(`https://sdkapi.hhlqilongzhu.cn/api/juhe_dgmusic?${qs.stringify({
-    key:'',
+    key:'Dragon0CD2C18C2F067049D0D075F0A55ADCB9',
     msg: '',
     n: 1,
     type: 'json',
