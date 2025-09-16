2e2953d81286a2bb5a01f4738703a8c4246d5292
赵孙超 <1493312415@qq.com>
2025-09-16 14:18:56 +0800

    fix: 修复视频分析中http链接未识别的问题

--- order/videoAnalysis.js
+++ order/videoAnalysis.js
@@ -6,7 +6,7 @@ export async function _sp_jx_Analysis(data, msg) {
   const isVideoAnalysis = config[data.group_id]?.isVideoAnalysis
   if (isVideoAnalysis) {
     const msg1 = msg[0]?.data?.text
-    if (msg1?.includes('https://')) {
+    if (msg1?.includes('https://')||msg1?.includes('http://')) {
       const urls = extractUrls(msg1);
       if (urls.length > 0) {
         const res = await sp_jx_Analysis({
