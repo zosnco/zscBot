d29ddfdb1003e053c657230b4d312f278308f986
赵孙超 <1493312415@qq.com>
2025-09-16 14:19:06 +0800

    1

--- order/videoAnalysis.js
+++ order/videoAnalysis.js
@@ -6,14 +6,14 @@ export async function _sp_jx_Analysis(data, msg) {
   const isVideoAnalysis = config[data.group_id]?.isVideoAnalysis
   if (isVideoAnalysis) {
     const msg1 = msg[0]?.data?.text
-    if (msg1?.includes('https://')||msg1?.includes('http://')) {
+    if (msg1?.includes('https://') || msg1?.includes('http://')) {
       const urls = extractUrls(msg1);
       if (urls.length > 0) {
         const res = await sp_jx_Analysis({
           url: urls[0], // 使用第一个找到的链接
         });
         const url = res?.data?.url
-       if(!url) return
+        if (!url) return
         messageTypeChange({
           type: "video",
           data: { file: url }
