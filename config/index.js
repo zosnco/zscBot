2488285d91edb269f187f622e69533e4a06a67ea
赵孙超 <1493312415@qq.com>
2025-10-14 10:31:03 +0800

    fix(config): 关闭默认聊天类型和自动回复功能
    将默认配置中的 chatType 从 'at' 改为空字符串，表示关闭聊天功能
    将 autoReplyProbability 从 5 和 100 降为 0，关闭自动回复功能
    为群组 985309959 添加缺失的视频解析和表情包配置

--- config/index.js
+++ config/index.js
@@ -4,23 +4,26 @@ const config = { // 白名单和配置
     "welcomeMessages": " 欢迎加入凌云 群名片改成游戏名字 记得留意下群公告内容谢谢", // 群欢迎语
     "isAntiRetraction": true, // 是否开启防撤回
     "isClockIn": true, // 是否开启签到
-    "chatType": 'at', // 空表示关闭，at表示at模式，name表示名字
+    "chatType": '', // 空表示关闭，at表示at模式，name表示名字
     "isVideoAnalysis": true, // 是否开启视频解析
-    "autoReplyProbability": 5, // 自动回复消息的概率，最高100%
+    "autoReplyProbability": 0, // 自动回复消息的概率，最高100%
     "isCardName": "LY.云端()", // 是否开启群名片修改
     "isEmoji": true, // 是否开启表情包
     // "sensitiveWords": sitiveWord // 敏感词
   },
-  "985309959": {},
+  "985309959": {
+    "isVideoAnalysis": true, // 是否开启视频解析
+    "isEmoji": true,
+  },
   "862794206": {},
   "1021173838": {
     "welcomeMessages": " 欢迎入群，嘻嘻",
     "isAntiRetraction": true,
     "isClockIn": true,
     "isEmoji": true,
-    "chatType": 'at', // 空表示关闭，at表示at模式，name表示名字
+    "chatType": '', // 空表示关闭，at表示at模式，name表示名字
     "isVideoAnalysis": true, // 是否开启视频解析
-    "autoReplyProbability": 100, // 自动回复消息的概率，最高100%
+    "autoReplyProbability": 0, // 自动回复消息的概率，最高100%
     "isCardName": "", // 是否开启群名片修改
     // "sensitiveWords": sitiveWord // 敏感词
   },
