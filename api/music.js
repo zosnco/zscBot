dd9cc38875b74c123d20bc88885a92068d9c3c23
赵孙超 <1493312415@qq.com>
2025-12-23 15:05:55 +0800

    feat(music): 添加音乐质量参数到腾讯音乐API请求
    在获取音乐URL的API请求中添加quality=7参数，以获取更高质量的音乐文件

--- api/music.js
+++ api/music.js
@@ -2,6 +2,6 @@ import qs from 'qs'; // qs.stringify(data)
 import { sendApiRequest } from '../utils/axiosEncapsulation.js'
 export async function dg_kugouSQ(data) {
   const res1 = await sendApiRequest(`https://api.vkeys.cn/v2/music/tencent/search/song?word=${data.msg}`)
-  const res2 = await sendApiRequest(`https://api.vkeys.cn/v2/music/tencent/geturl?id=${res1.data[data.n-1].id}`)
+  const res2 = await sendApiRequest(`https://api.vkeys.cn/v2/music/tencent/geturl?id=${res1.data[data.n-1].id}&quality=7`)
   return res2
 }
