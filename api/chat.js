ed79bbaff6e5131d726798c0b4b1a2f66852a5f9
赵孙超 <1493312415@qq.com>
2025-12-23 13:03:06 +0800

    chore: 更新服务器IP地址至43.142.191.93

--- api/chat.js
+++ api/chat.js
@@ -2,11 +2,11 @@
 import { sendApiRequest } from '../utils/axiosEncapsulation.js'// gjlpchant
 // 发送消息
 export async function gjlpChat(data) {
-  const res = await sendApiRequest('http://117.72.118.144:5001/api/chat', 'post', data)
+  const res = await sendApiRequest('http://43.142.191.93:5001/api/chat', 'post', data)
   return res
 }
 // 清空上下文
 export async function gjlpClearContext(data) {
-  const res = await sendApiRequest('http://117.72.118.144:5001/api/clear-context', 'post', data)
+  const res = await sendApiRequest('http://43.142.191.93:5001/api/clear-context', 'post', data)
   return res
 }
--- api/qqBot.js
+++ api/qqBot.js
@@ -2,32 +2,32 @@ import qs from 'qs'; // qs.stringify(data)
 import { sendApiRequest } from '../utils/axiosEncapsulation.js'
 // 发送群消息
 export async function send_group_msg(data) {
-  const res = await sendApiRequest(`http://117.72.118.144:3010/send_group_msg`, 'POST', qs.stringify(data))
+  const res = await sendApiRequest(`http://43.142.191.93:3010/send_group_msg`, 'POST', qs.stringify(data))
   return res
 }
 // 发送私聊消息
 export async function send_private_msg(data) {
-  const res = await sendApiRequest(`http://117.72.118.144:3010/send_private_msg`, 'POST', qs.stringify(data))
+  const res = await sendApiRequest(`http://43.142.191.93:3010/send_private_msg`, 'POST', qs.stringify(data))
   return res
 }
 // 合并消息转发
 export async function send_forward_msg(data) {
-  const res = await sendApiRequest(`http://117.72.118.144:3010/send_forward_msg`, 'POST', qs.stringify(data))
+  const res = await sendApiRequest(`http://43.142.191.93:3010/send_forward_msg`, 'POST', qs.stringify(data))
   return res
 }
 // 群签到
 export async function send_group_sign(data) {
-  const res = await sendApiRequest(`http://117.72.118.144:3010/send_group_sign`, 'POST', qs.stringify(data))
+  const res = await sendApiRequest(`http://43.142.191.93:3010/send_group_sign`, 'POST', qs.stringify(data))
   return res
 }
 // 撤回消息
 export async function delete_msg(data) {
-  const res = await sendApiRequest(`http://117.72.118.144:3010/delete_msg`, 'POST', qs.stringify(data))
+  const res = await sendApiRequest(`http://43.142.191.93:3010/delete_msg`, 'POST', qs.stringify(data))
   return res
 }
 
 // 修改群名片
 export async function set_group_card(data) {
-  const res = await sendApiRequest(`http://117.72.118.144:3010/set_group_card`, 'POST', qs.stringify(data))
+  const res = await sendApiRequest(`http://43.142.191.93:3010/set_group_card`, 'POST', qs.stringify(data))
   return res
 }
